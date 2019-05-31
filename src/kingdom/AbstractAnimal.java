9120ebae76fed8b94c7509764482ab9bebaaa7cf
Nathan Thomas <nwthomas@me.com>
2019-05-31 00:38:27 -0700

    Build out AbstractAnimal.java abstract class

---
+++ src/kingdom/AbstractAnimal.java
@@ -0,0 +1,23 @@
+package kingdom;
+
+public abstract class AbstractAnimal
+{
+	private static int maxId = 0;
+
+	public AbstractAnimal(String name, int year)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+		this.year = year;
+	}
+
+	public String eat()
+	{
+		return "The " + name + " ate some food.";
+	}
+
+	public abstract String move();
+	public abstract String breath();
+	public abstract String reproduce();
+}
---
+++ src/kingdom/Main.java
@@ -0,0 +1,9 @@
+package kingdom;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
