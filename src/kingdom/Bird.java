9d9bd4613aae838c41d120334789f0d1179b6549
Nathan Thomas <nwthomas@me.com>
2019-05-31 00:46:48 -0700

    Make Bird.java and Fish.java extended classes

--- src/kingdom/Bird.java
+++ src/kingdom/Bird.java
@@ -1,1 +1,28 @@
 package kingdom;
+
+public class Bird extends AbstractAnimal
+{
+	public Bird(String name, int year)
+	{
+		this.moveMethod = "fly";
+		this.breathMethod = "lungs";
+		this.reproduceMethod = "eggs";
+	}
+
+	@Override
+	public String move() {
+		return "The movement method of " + this.name + " is " + this.moveMethod;
+	}
+
+	@Override
+	public String breath()
+	{
+		return "The breathing method of " + this.name + " is " + this.breathMethod;
+	}
+
+	@Override
+	public String reproduce()
+	{
+		return "The reproduction method of " + this.name + " is " + this.reproduceMethod;
+	}
+}
--- src/kingdom/Fish.java
+++ src/kingdom/Fish.java
@@ -1,1 +1,28 @@
 package kingdom;
+
+public class Fish extends AbstractAnimal
+{
+	public Fish(String name, int year)
+	{
+		this.moveMethod = "swim";
+		this.breathMethod = "gills";
+		this.reproduceMethod = "eggs";
+	}
+
+	@Override
+	public String move() {
+		return "The movement method of " + this.name + " is " + this.moveMethod;
+	}
+
+	@Override
+	public String breath()
+	{
+		return "The breathing method of " + this.name + " is " + this.breathMethod;
+	}
+
+	@Override
+	public String reproduce()
+	{
+		return "The reproduction method of " + this.name + " is " + this.reproduceMethod;
+	}
+}
